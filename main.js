013423dd3789c877cef2140e8133b7f7a00f1097
Patrick Aondona <aondopat@gmail.com>
2023-04-17 21:22:41 +0100

    PalindromeChecker

--- main.js
+++ main.js
@@ -3,13 +3,20 @@ let checkButton = document.querySelector("button");
 let response = document.getElementById("response");
 
 function isPalindrome() {
-    let forwardValue = inputItem.value;
-    let reverseValue = inputItem.value.split('').reverse().join('');
-    if( forwardValue== reverseValue){
-        response.innerText = (`${forwardValue} is a palindrome.`);    
-    }
+    if (inputItem.value == ""){
+        alert("Please input a value");
+    } 
     else{
-        response.innerText = (`${forwardValue} is not a palindrome.`);
+        let forwardValue = inputItem.value;
+        let reverseValue = inputItem.value.split('').reverse().join('');
+        if( forwardValue == reverseValue){
+            response.innerText = (`"${forwardValue}" is a palindrome.`);    
+        }
+        else{
+            response.innerText = (`"${forwardValue}" is not a palindrome.`);
+        }
     }
+    inputItem.value = "";
 }
 checkButton.addEventListener("click", isPalindrome);
+
